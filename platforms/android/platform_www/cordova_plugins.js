a227a020602a596a2c6558589ab7075ab8f4fefc
Vasily <just.one.man@yandex.ru>
2018-10-22 18:34:04 +0300

    Tried adding google services cordova plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,10 +1,31 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "acidhax.cordova.chromecast.ChromecastApi",
+    "file": "plugins/acidhax.cordova.chromecast/chrome.cast.js",
+    "pluginId": "acidhax.cordova.chromecast",
+    "clobbers": [
+      "chrome.cast"
+    ]
+  },
+  {
+    "id": "acidhax.cordova.chromecast.EventEmitter",
+    "file": "plugins/acidhax.cordova.chromecast/EventEmitter.js",
+    "pluginId": "acidhax.cordova.chromecast"
+  },
+  {
+    "id": "acidhax.cordova.chromecast.tests",
+    "file": "plugins/acidhax.cordova.chromecast/tests/tests.js",
+    "pluginId": "acidhax.cordova.chromecast"
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-crosswalk-webview": "2.4.0",
-  "cordova-android-support-gradle-release": "1.4.5"
+  "cordova-android-support-gradle-release": "1.4.5",
+  "cordova-support-google-services": "1.2.1",
+  "acidhax.cordova.chromecast": "0.0.1-alpha"
 };
 // BOTTOM OF METADATA
 });
